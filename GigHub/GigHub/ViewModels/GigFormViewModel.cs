08caf80a53ed5b617919e113fc0c120435b165ef
shakil4014008 <shakil4014008@gmail.com>
2019-01-27 00:50:53 -0500

    Upload practice social networking site first use case

---
+++ GigHub/GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,32 @@
+﻿using GigHub.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        [Required]
+        public string Venue { get; set; }
+
+        [Required]
+        [FutureDateViewModel]
+        public string Date { get; set; }
+
+        [Required]
+        [ValidTimeViewModel]
+        public string Time { get; set; }
+
+        [Required]
+        public byte Genre { get; set; }
+
+ 
+        public IEnumerable<Genre> Genres { get; set; }
+
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}

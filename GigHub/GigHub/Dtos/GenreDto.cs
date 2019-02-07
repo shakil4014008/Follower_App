13de0d3f881b6e76b4751838f6fb7cd679fb95c3
shakil4014008 <shakil4014008@gmail.com>
2019-02-07 18:55:37 -0500

    creating contracts of api,  creating Dtos, Mapping domain objects with automapper

---
+++ GigHub/GigHub/Dtos/GenreDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Dtos
+{
+    public class GenreDto
+    {
+        public byte Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+    }
+}

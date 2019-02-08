3127789486318addb0003d63b7befacff362371b
shakil4014008 <shakil4014008@gmail.com>
2019-02-08 18:07:44 -0500

    Search implementation, css and refactoring.

--- GigHub/GigHub/Controllers/GigsController.cs
+++ GigHub/GigHub/Controllers/GigsController.cs
@@ -142,5 +142,11 @@ namespace GigHub.Controllers
             return RedirectToAction("Mine", "Gigs");
 
         }
+
+        [HttpPost]
+        public ActionResult Search(GigsViewModel viewModel)
+        {
+            return RedirectToAction("Index", "Home", new {query = viewModel.SearchTerm});
+        }
     }
 }
